2bf4b9d353234e3ed7398572ed7cfec20e3e36a7
venugopalvaleti <venu.ky183@gmail.com>
2025-04-08 03:21:08 +0000

    Updated populate_db.py to use pymongo for direct MongoDB operations and resolved compatibility issues.

--- octofit-tracker/backend/tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/tracker/management/commands/populate_db.py
@@ -1,54 +1,57 @@
 from django.core.management.base import BaseCommand
-from tracker.models import User, Team, Activity, Leaderboard, Workout
+from pymongo import MongoClient
+from django.conf import settings
 from bson import ObjectId
-from datetime import timedelta
 
 class Command(BaseCommand):
     help = 'Populate the database with test data for users, teams, activity, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
-        # Clear existing data
-        User.objects.all().delete()
-        Team.objects.all().delete()
-        Activity.objects.all().delete()
-        Leaderboard.objects.all().delete()
-        Workout.objects.all().delete()
+        # Connect to MongoDB
+        client = MongoClient(settings.DATABASES['default']['HOST'], settings.DATABASES['default']['PORT'])
+        db = client[settings.DATABASES['default']['NAME']]
 
-        # Create users
+        # Clear existing collections
+        db.users.delete_many({})
+        db.teams.delete_many({})
+        db.activity.delete_many({})
+        db.leaderboard.delete_many({})
+        db.workouts.delete_many({})
+
+        # Insert users
         users = [
-            User(_id=ObjectId(), username='user1', email='user1@example.com', password='password1'),
-            User(_id=ObjectId(), username='user2', email='user2@example.com', password='password2'),
-            User(_id=ObjectId(), username='user3', email='user3@example.com', password='password3'),
+            {"_id": ObjectId(), "username": "user1", "email": "user1@example.com", "password": "password1"},
+            {"_id": ObjectId(), "username": "user2", "email": "user2@example.com", "password": "password2"},
+            {"_id": ObjectId(), "username": "user3", "email": "user3@example.com", "password": "password3"},
         ]
-        User.objects.bulk_create(users)
+        db.users.insert_many(users)
 
-        # Create teams
-        team = Team(_id=ObjectId(), name='Team A')
-        team.save()
-        team.members.add(*users)
+        # Insert team
+        team = {"_id": ObjectId(), "name": "Team A", "members": [user["_id"] for user in users]}
+        db.teams.insert_one(team)
 
-        # Create activities
+        # Insert activities
         activities = [
-            Activity(_id=ObjectId(), user=users[0], activity_type='Running', duration=timedelta(minutes=30)),
-            Activity(_id=ObjectId(), user=users[1], activity_type='Cycling', duration=timedelta(minutes=45)),
-            Activity(_id=ObjectId(), user=users[2], activity_type='Swimming', duration=timedelta(minutes=60)),
+            {"_id": ObjectId(), "user": users[0]["_id"], "activity_type": "Running", "duration": 30},
+            {"_id": ObjectId(), "user": users[1]["_id"], "activity_type": "Cycling", "duration": 45},
+            {"_id": ObjectId(), "user": users[2]["_id"], "activity_type": "Swimming", "duration": 60},
         ]
-        Activity.objects.bulk_create(activities)
+        db.activity.insert_many(activities)
 
-        # Create leaderboard entries
+        # Insert leaderboard entries
         leaderboard_entries = [
-            Leaderboard(_id=ObjectId(), user=users[0], score=100),
-            Leaderboard(_id=ObjectId(), user=users[1], score=90),
-            Leaderboard(_id=ObjectId(), user=users[2], score=80),
+            {"_id": ObjectId(), "user": users[0]["_id"], "score": 100},
+            {"_id": ObjectId(), "user": users[1]["_id"], "score": 90},
+            {"_id": ObjectId(), "user": users[2]["_id"], "score": 80},
         ]
-        Leaderboard.objects.bulk_create(leaderboard_entries)
+        db.leaderboard.insert_many(leaderboard_entries)
 
-        # Create workouts
+        # Insert workouts
         workouts = [
-            Workout(_id=ObjectId(), name='Workout 1', description='Description for Workout 1'),
-            Workout(_id=ObjectId(), name='Workout 2', description='Description for Workout 2'),
-            Workout(_id=ObjectId(), name='Workout 3', description='Description for Workout 3'),
+            {"_id": ObjectId(), "name": "Workout 1", "description": "Description for Workout 1"},
+            {"_id": ObjectId(), "name": "Workout 2", "description": "Description for Workout 2"},
+            {"_id": ObjectId(), "name": "Workout 3", "description": "Description for Workout 3"},
         ]
-        Workout.objects.bulk_create(workouts)
+        db.workouts.insert_many(workouts)
 
-        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data using pymongo.'))
